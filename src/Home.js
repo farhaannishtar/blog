0be0124b3bc135d45f7eb48cc4aa35e11e779f23
Farhaan Nishtar <farhaanmnishtar@gmail.com>
2021-12-27 06:23:10 +0530

    delete blog

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
+  const [name, setName] = useState('Shehan');
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -13,9 +14,15 @@ const Home = () => {
     setBlogs(newBlogs);
   } 
 
+  useEffect(() => {
+    console.log('useEffect ran');
+  }, [name])
+
   return (
       <div className="home">
           <BlogList blogs={blogs} title='Will not give up' deleteBlog={deleteBlog}  />
+          <button onClick={ () => setName('Shiraz') }>Change Name</button>
+          <p>{ name }</p>
       </div>
   );
 }
